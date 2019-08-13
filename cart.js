46dc1f2dccbf614dc3828c79dd1ade1324bf4505
Esteban Hernandez <contact@mksmdesign.com>
2019-08-13 14:32:37 -0600

    Completed afternoon Jest project

--- cart.js
+++ cart.js
@@ -1,18 +1,21 @@
 const cars = require('./data/cars');
 
 module.exports = {
-  cart: null,
-  total: null,
+  cart: [],
+  total: 0,
 
-  addToCart: function() {
-
+  addToCart: function(carToAdd) {
+    this.cart.push(carToAdd);
+    this.total += carToAdd.price;
   },
 
-  removeFromCart: function() {
-
+  removeFromCart: function(indexToRemove, carPrice) {
+    this.cart.splice(indexToRemove, 1);
+    this.total -= carPrice;
   },
   
   checkout: function() {
-
+    this.cart = [];
+    this.total = 0;
   }
 };
